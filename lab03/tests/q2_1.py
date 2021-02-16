852aaa9819f8b11ad9e189f04a6b9fca8bc7de33
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2021-02-16 10:34:40 -0500

    Lab 03 tests

---
+++ lab03/tests/q2_1.py
@@ -0,0 +1,10 @@
+test = {   'name': 'q2_1',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': ">>> 'Day' in list(calls.columns)\nTrue", 'hidden': False, 'locked': False},
+                                   {   'code': ">>> ['Wednesday', 'Friday', 'Tuesday', 'Saturday', 'Monday', 'Sunday', 'Thursday'] == list(calls['Day'].unique())\nTrue",
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab03/tests/q2_7.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q2_7',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': ">>> 'Hour' in list(calls.columns)\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> calls['Hour'].sum() == 50601\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> calls['Hour'].dtypes == 'int64'\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
