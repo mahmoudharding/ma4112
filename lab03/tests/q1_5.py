60f6d43876dcba3b5e2c8c945b5eba26b28be0ba
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2021-02-16 11:18:42 -0500

    Updated Lab 03

---
+++ lab03/tests/q1_5.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q1_5',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> type(offense_larceny) == list\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> len(offense_larceny) == 3\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab03/tests/q1_6.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q1_6',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> type(cvlengend_types) == list\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> len(cvlengend_types) == 5\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
