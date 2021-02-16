de68d85e0c57f9c6651b0672f2b01fb249666907
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2021-02-16 11:12:36 -0500

    Deleted files

--- lab03/tests/q1_4.py
+++
@@ -1,7 +0,0 @@
-test = {   'name': 'q1_4',
-    'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> calls_by_cvlegend_and_offense["LARCENY", "THEFT FROM PERSON"] == 24\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
--- lab03/tests/q1_6.py
+++
@@ -1,8 +0,0 @@
-test = {   'name': 'q1_6',
-    'points': 1,
-    'suites': [   {   'cases': [   {'code': '>>> type(cvlengend_types) == list\nTrue', 'hidden': False, 'locked': False},
-                                   {'code': '>>> len(cvlengend_types) == 5\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
