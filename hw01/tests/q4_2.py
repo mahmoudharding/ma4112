37664ce38170a37ddbe8eba89281b38091f1792e
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2021-03-02 19:44:09 -0500

    Update hw01

--- hw01/tests/q4_2.py
+++ hw01/tests/q4_2.py
@@ -1,7 +1,6 @@
 test = {   'name': 'q4_2',
     'points': 1,
     'suites': [   {   'cases': [   {'code': ">>> 1 < sum(bike['fatemp']) < 1193549\nTrue", 'hidden': False, 'locked': False},
-                                   {'code': '>>> bike.shape[1] == 19\nTrue', 'hidden': False, 'locked': False},
                                    {'code': ">>> 'fatemp' in bike.columns\nTrue", 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
