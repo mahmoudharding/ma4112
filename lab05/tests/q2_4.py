4832ce76414cdef5049a053509cd31ba40001698
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2021-03-09 11:31:35 -0500

    Add files via upload

--- lab05/tests/q2_4.py
+++ lab05/tests/q2_4.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q2_4',
     'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> isinstance(q2_4, np.float64)\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> 0 < q2_4 < .8\nTrue', 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> isinstance(q2_4, np.float64)\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> round(q2_4, 2) < .86\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
