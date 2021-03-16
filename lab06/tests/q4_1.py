ef790272a74f00faedaf339a00b340ea48184b6b
mahmoudharding <55882955+mahmoudharding@users.noreply.github.com>
2021-03-16 07:04:59 -0400

    Update Lab 06

---
+++ lab06/tests/q4_1.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q4_1',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> len(penguin_species) == 3\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ lab06/tests/q4_2.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q4_2',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> type(p_a) == pd.core.frame.DataFrame\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> type(p_c) == pd.core.frame.DataFrame\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> type(p_g) == pd.core.frame.DataFrame\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
